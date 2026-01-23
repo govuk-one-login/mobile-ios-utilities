b3a10f5d4655a2580f29df4b597c1330633e577a
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-01-23 11:52:36 +0000

    chore: initial set up (#1)
    * chore: initial set up
    
    * chore: add workflow files
    
    * chore: format

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "GDSUtilities",
+    products: [
+        .library(
+            name: "GDSUtilities",
+            targets: ["GDSUtilities"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/SimplyDanny/SwiftLintPlugins",
+            .upToNextMajor(from: "0.58.2")
+        )
+    ],
+    targets: [
+        .target(
+            name: "GDSUtilities",
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
+        ),
+        .testTarget(
+            name: "GDSUtilitiesTests",
+            dependencies: ["GDSUtilities"]
+        )
+    ]
+)
