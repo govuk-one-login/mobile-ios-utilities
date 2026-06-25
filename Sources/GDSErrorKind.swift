97c0fcecc131ecf3ce5568db89a8b1ee96883cb6
Markos Zoulias Charatzas <markos.charatzas@dsit.gov.uk>
2026-06-25 18:16:08 +0100

    Revert "Merge pull request #12 from govuk-one-login/feat/DCMAW-21051-ensure-errors-are-consistent-across-releases"
    This reverts commit e81ec582b5d3e063af5391b61e96cc2bbbe2bad9.

--- Sources/GDSErrorKind.swift
+++ Sources/GDSErrorKind.swift
@@ -1,4 +1,18 @@
+// does not conform to `CustomStringConvertable` or `CustomDebugStringConvertable` to avoid infinite loop
 public protocol GDSErrorKind: Sendable,
-                              CustomStringConvertible,
                               RawRepresentable,
-                              Equatable where RawValue == Int { }
+                              Equatable where RawValue == String { }
+
+extension GDSErrorKind {
+    public var description: String {
+        rawValue == "\(self)" ?
+            "\(self)" :
+            "\(self) - \(rawValue)"
+    }
+}
+
+extension GDSErrorKind where Self.RawValue == String {
+    public var localizedDescription: String {
+        self.rawValue
+    }
+}
