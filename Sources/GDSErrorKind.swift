a2f19bf5cb45447c662d4f4732da7a1dcf2306e3
Markos Zoulias Charatzas <Markos.Charatzas@dsit.gov.uk>
2026-07-10 14:03:35 +0100

    feat!: DCMAW-21051 a GDSError now has a constant error code (#17)
    Previously, the error code for a `GDSError` was dynamic based on the
    	`line` where the calling code created/raised it. This carried a risk that an error code
    	would randomly change over time based on changes to the code. This made error tracking
    	difficult to track and identify.
    
    	Now, `GDSError` will instead compute its error code based on the `GDSErrorKind` which is
    	defined as an `Int`. That requires and expects any `GDSErrorKind` errors to have their error code
    	set as part of their declaration thus constant.
    
    	Whereas previously a `GDSErrorKind` error was defined based on a String representation, which was also
    	used for its description, the requirement to have a description for a `GDSErrorKind` is now formalised by adopting
    	the `CustomStringConvertable` protocol. It’s also worth noting that the description of an enum, which the `GDSErrorKind`
    	seems to be designed for and adopted by, used “reflection” to dynamically generate the description of an enum.
    	This implementation carries 1 risks and violated 1 principle. The description property is what `String(describing:)`, and
    	equivalent like String interpolation, may use to compute its description. Hence the warning on infinite recursion which
    	has now been removed. A enum with a rawValue should be able to be initialised from its rawValue.
    	The description property created a couple with the rawValue that not only violated the construction of a String typed
    	enum but also relied on how an enum is converting into a String by observation, not a public contract, thus subject to change.

--- Sources/GDSErrorKind.swift
+++ Sources/GDSErrorKind.swift
@@ -1,18 +1,4 @@
-// does not conform to `CustomStringConvertable` or `CustomDebugStringConvertable` to avoid infinite loop
 public protocol GDSErrorKind: Sendable,
+                              CustomStringConvertible,
                               RawRepresentable,
-                              Equatable where RawValue == String { }
-
-extension GDSErrorKind {
-    public var description: String {
-        rawValue == "\(self)" ?
-            "\(self)" :
-            "\(self) - \(rawValue)"
-    }
-}
-
-extension GDSErrorKind where Self.RawValue == String {
-    public var localizedDescription: String {
-        self.rawValue
-    }
-}
+                              Equatable where RawValue == Int { }
